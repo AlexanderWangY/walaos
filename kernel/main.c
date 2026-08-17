87f96eb1bdbd9617eb3945a7d690a23a5ca2a98f
AlexanderWangY <alexander.yisu.wang@outlook.com>
2026-08-17 15:49:39 -0700

    hart id and device tree in main args

--- kernel/main.c
+++ kernel/main.c
@@ -1,6 +1,7 @@
 // kernel main entry point, but uses architecture compiled from arch/
 
-void kmain(void) {
+// From a0, and a1 respectively, DO NOT TOUCH a0 OR a1 in entry.s. DO NOT DO NOT DO NOT
+void kmain(unsigned long hart_id, unsigned long dtb) {
   for (;;) {
     // HALT
   }
