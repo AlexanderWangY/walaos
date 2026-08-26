6e81ce478409366da183cfe7dddd1d81b1b071d0
AlexanderWangY <alexander.yisu.wang@outlook.com>
2026-08-25 22:37:48 -0500

    feat: enable plic for uart0

--- kernel/drivers/irq/plic.c
+++ kernel/drivers/irq/plic.c
@@ -1,10 +1,16 @@
 #include <irq/plic.h>
 #include <stdint.h>
 
+// Sets the context's priority threshold to 0
 void plic_init(uintptr_t base, unsigned int context) {
   *(volatile uint32_t *)(base + 0x200000 + context * 0x1000) = 0;
 }
 
+// Set the enable bit of the irq to 1 and priority
 void plic_enable(uintptr_t base, unsigned int context, unsigned int irq, unsigned int priority) {
-  
+  volatile uint32_t *enable = (volatile uint32_t *)(base + 0x002000 + context * 0x80 + (irq / 32) * 4);
+  *enable |= (1U << (irq % 32));
+  *(volatile uint32_t *)(base + irq * 4) = priority;
 }
+
+
--- platform/qemu-virt/platform.c
+++ platform/qemu-virt/platform.c
@@ -1,13 +1,27 @@
 #include <platform.h>
 #include <uart/ns16550.h>
+#include <irq/plic.h>
 
 #define UART0 0x10000000UL
+#define UART0_IRQ 10
+#define UART0_PRIORITY 1
+#define PLIC_BASE 0x0c000000UL
+
+#define MHART0_CTX 0
+#define SHART0_CTX 1
+#define MHART1_CTX 2
+#define SHART1_CTX 3
+// Add more mharts and sharts as we go
+// Haha, shart is a funny word... poop
 
 const char *platform_name(void) {
   return "qemu-virt";
 }
 
 void platform_init(void) {
+    // Enable supervisor hart 0 plic
+    plic_init(PLIC_BASE, SHART0_CTX);
+    plic_enable(PLIC_BASE, SHART0_CTX, UART0_IRQ, UART0_PRIORITY);
     ns16550_init(UART0);
 };
 
