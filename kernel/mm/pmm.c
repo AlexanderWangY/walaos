e330faf14481b3cf355ccac0a2ddb026bd005e3d
AlexanderWangY <alexander.yisu.wang@outlook.com>
2026-09-02 00:45:51 -0500

    feat: msb bitmap allocation + free pages!

--- kernel/mm/pmm.c
+++ kernel/mm/pmm.c
@@ -70,13 +70,30 @@ void init_pmm(uintptr_t start, uintptr_t end, uint64_t page_size) {
   // Mark invalid trailing bits as full so they never get allocated
   uint64_t valid_bits = pmm.page_count % 64;
   if (valid_bits != 0)
-    pmm.bitmap[pmm.bitmap_size - 1] = UINT64_MAX << valid_bits;
+    pmm.bitmap[pmm.bitmap_size - 1] = UINT64_MAX >> valid_bits;
 
   klog(INFO,
        "PMM initialized: bitmap [0x%lX, 0x%lX), allocatable [0x%lX, 0x%lX)\n",
        (uintptr_t)pmm.bitmap, pmm.start, pmm.start, pmm.end);
 }
 
+
+/*
+We allocate using a bitmap.
+
+A bitmap consists of an array of uint64_t, each bit in the uint64_t represents one page.
+Therefore 1 bitmap entry (uint64_t) track 64 page entries, at 4KiB per page its 256KiB tracked per bitmap entry.
+
+As for the bitmap entry itself, it is allocated from the first 0 (free) MSB.
+So we allocate from right to left.
+
+Example:
+
+Bitmap entry index = 10 (just arbitrarily chosen)
+1111 1111 0111 1001 .....
+          ^
+          This is the page to be allocated.
+*/
 uintptr_t alloc_pmm(void) {
   uint64_t idx = pmm.bitmap_size;
   for (uint64_t i = 0; i < pmm.bitmap_size; i++) {
@@ -91,13 +108,52 @@ uintptr_t alloc_pmm(void) {
 
   uint64_t bitfield = pmm.bitmap[idx];
 
-  for (int i = 63; i >= 0; i--) {
-    if (!(bitfield & (1ULL << i))) {
-      pmm.bitmap[idx] |= 1ULL << i;
-      uint64_t page_index = idx * 64 + (uint64_t)i;
+  for (uint64_t i = 0; i < 64; i++) {
+    uint64_t bit = 63 - i;
+    
+    if (!(bitfield & (1ULL << bit))) {
+      pmm.bitmap[idx] |= 1ULL << bit;
+      uint64_t page_index = idx * 64 + i;
       return pmm.start + page_index * pmm.page_size;
     }
   }
 
   return 0;
+}
+
+void pmm_free_page(uintptr_t pa) {
+  if (pa < pmm.start || pa >= pmm.end) {
+    klog(WARN, "pmm tried to free 0x%lX which is out of bounds\n", pa);
+    return;
+  }
+
+  if ((pa - pmm.start) % pmm.page_size != 0) {
+    klog(WARN, "pmm can't free non page size aligned address 0x%lX\n", pa);
+    return;
+  }
+
+  // Calculate bit position
+  uint64_t page_idx = (pa - pmm.start) / pmm.page_size;
+  uint64_t bitmap_idx = page_idx / 64;
+  uint64_t bit = 63 - (page_idx % 64);
+
+  if (bitmap_idx >= pmm.bitmap_size) {
+    klog(WARN, "pmm tried free from bitmap idx %d but it is out of bounds\n", bitmap_idx);
+    return;
+  }
+
+
+  // Already free!
+  if (!(pmm.bitmap[bitmap_idx] & (1ULL << bit)))
+    return;
+
+  // Otherwise clear the page and free
+  // At this point the page is 4K aligned
+  uint64_t *p = (uint64_t *)pa;
+  for (uint64_t i = 0; i < pmm.page_size / sizeof(uint64_t); i++) {
+    p[i] = 0;
+  }
+
+  // clear bit
+  pmm.bitmap[bitmap_idx] &= ~(1ULL << bit);
 }
