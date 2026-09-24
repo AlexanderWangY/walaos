1e09ce648dfee40f776df4789f8727f57a7c4976
AlexanderWangY <alexander.yisu.wang@outlook.com>
2026-09-23 21:45:20 -0500

    fix wrong kmalloc ordering and calculations

--- kernel/main.c
+++ kernel/main.c
@@ -3,6 +3,7 @@
 // From a0, and a1 respectively, DO NOT TOUCH a0 OR a1 in entry.s. DO NOT DO NOT DO NOT
 #include "pmm.h"
 #include "vmm.h"
+#include <kheap.h>
 #include <console.h>
 #include <platform.h>
 #include <stdint.h>
@@ -31,6 +32,11 @@ void kmain(unsigned long hart_id, unsigned long dtb) {
     kprintf("direct-map value: 0x%lX\n", direct[0]);
 
     vmm_map_page(0x60000000, pa, PTE_R | PTE_W);
+
+    // Attempt to allocate kheap
+    char *c = kmalloc(10);
+    c[0] = 'A';
+    
     for (;;)
         __asm__ volatile ("wfi");
 }
--- kernel/mm/kheap.c
+++ kernel/mm/kheap.c
@@ -23,34 +23,39 @@ static kblock_t *kheap;
 
 void kheap_init(void) {
   uintptr_t pa = pmm_alloc_page();
+  uintptr_t va = pa_to_va(pa);
+  vmm_map_page(va, pa, PTE_R | PTE_W | PTE_X);
 
-  kheap = (kblock_t*)pa_to_va(pa);
+  kheap = (kblock_t*)va;
   kheap->free = true;
   kheap->next = NULL;
   kheap->prev = NULL;
   kheap->size = PAGE_SIZE - align_up_16(sizeof(kblock_t));
+
 }
 
 void *kmalloc(size_t size) {
-
   kblock_t *curr = kheap;
 
-  while (curr && (!curr->free || curr->size >= (align_up_16(size) + HEADER_SIZE)))
+  while (curr && (!curr->free || curr->size < align_up_16(size)))
     curr = curr->next;
 
   if (curr == NULL)
     return NULL;
-
-  size_t remaining = curr->size - (align_up_16(size) + HEADER_SIZE);
+  
+  size_t remaining = curr->size - align_up_16(size);
   if (remaining > HEADER_SIZE + 16) {
     // Allocated and split
 
-    kblock_t *new_block = (kblock_t *)(unsigned char *)curr + HEADER_SIZE + curr->size;
+    kblock_t *new_block = (kblock_t *)((unsigned char *)curr + HEADER_SIZE + align_up_16(size));
 
     new_block->size = remaining - HEADER_SIZE;
     new_block->free = true;
     new_block->next = curr->next;
     new_block->prev = curr;
+
+    if (new_block->next)
+      new_block->next->prev = new_block;
 
     curr->free = false;
     curr->size = align_up_16(size);
@@ -64,4 +69,6 @@ void *kmalloc(size_t size) {
   }  
 }
 
-void kfree(void *ptr) {}
+void kfree(void *ptr) {
+  
+}
